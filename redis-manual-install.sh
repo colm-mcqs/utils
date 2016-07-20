24671d52d7b62b5744287b2acb4d231a493df0fb
Colm O'Shea <colm-mcqs@users.noreply.github.com>
2016-07-20 12:18:45 +0100

    Add redis-manual-install.sh
    Added manual installation script for Redis server on Ubuntu 14.04. 
    
    The user still needs to confirm ports, etc.

---
+++ redis-manual-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#install redis
+sudo apt-get install tcl8.5
+wget http://download.redis.io/releases/redis-stable.tar.gz
+tar xzf redis-stable.tar.gz
+cd redis-stable
+make
+make test
+sudo make install
+cd utils
+sudo ./install_server.sh -y
